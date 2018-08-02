6e75abcec16047b660ef4684152e3c9a5ea16e6a
silvia-YQY <545067444@qq.com>
2018-08-03 00:02:06 +0800

    第一次react应用

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+let div = document.getElementById('root')
+ReactDOM.render(<App />, div);
+console.log('88888')
+// registerServiceWorker();

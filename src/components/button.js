ee002042ed40193a5bed06b2f8ae5dc6d9ed3717
silvia-YQY <545067444@qq.com>
2018-08-12 12:03:57 +0800

    添加button组件，拥有涟漪效果

--- src/components/button.js
+++ src/components/button.js
@@ -2,11 +2,48 @@ import React, { Component } from 'react';
 import './button.css'
 
 class Button extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      acitve: false,
+      deltaX: 0,
+      deltaY: 0
+    };
+    this.myRef = React.createRef();
+  }
   render() {
-    return ( 
-      <button className="button2">{this.props.value} </button>
+    return (
+      <button ref={this.myRef} className="button2" onClick={this.x.bind(this)}>
+        <span className="value">{this.props.value} </span>
+        {/* {this.props.value} */}
+        {this.state.active === true ? (
+        <span className="circle"
+          onAnimationEnd={this.y.bind(this)}
+          style={{ left: this.state.deltaX, top: this.state.deltaY }}
+        /> ): (
+          ''
+          )}
+      </button>
     );
   }
-} 
+  x(event) {
+    let { x, y } = this.myRef.current.getBoundingClientRect()
+    let { clientX, clientY } = event
+
+    let deltaX = clientX - x - 5
+    let deltaY = clientY - y - 5
+    console.log('x,y', deltaX, deltaY)
+    this.setState({
+      active: true,
+      deltaX: deltaX,
+      deltaY: deltaY
+    })
+  }
+  y(){
+    this.setState({
+      active: false,
+    })
+  }
+}
 
 export default Button;
